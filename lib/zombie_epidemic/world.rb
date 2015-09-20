42ab070c0ccef31e3cb3dee33df3986a92266bc4
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2015-09-20 13:28:43 -0300

    Simulation could be lively displayed in terminal
    this is now the default behavior

--- lib/zombie_epidemic/world.rb
+++ lib/zombie_epidemic/world.rb
@@ -1,6 +1,6 @@
 module ZombieEpidemic
   class World
-    def initialize(population_size = 10, map_klass = Map, agent_klass = Agent, stm_klass = StateTransitionMachine)
+    def initialize(population_size = 10, map_klass = Map, agent_klass = Agent)
       @map = map_klass.new(10, 10)
       @stm = StateTransitionMachine.new
       @agents = []
@@ -8,13 +8,13 @@ module ZombieEpidemic
       @time = 0
     end
 
-    def run(steps = 24 * 3600 * 2 * 10)
+    def run(steps: 24 * 3600 * 2 * 10, output_on_console: false)
       dirname = "zombie_epidemic_#{Time.now.utc.to_i}"
       Dir.mkdir(dirname)
 
       digits = steps.to_s.length
 
-      render(dirname, digits)
+      render(dirname, digits, 10, output_on_console)
 
       steps.times do
         @time += 1
@@ -26,7 +26,7 @@ module ZombieEpidemic
         @agents.each_with_index do |agent, index|
           agent.commit
         end
-        render(dirname, digits)
+        render(dirname, digits, 10, output_on_console)
         deads = @agents.select{ |agent| agent.state.name == :dead }
         @agents.reject!{ |agent| agent.state.name == :dead }
         if deads
@@ -42,16 +42,21 @@ module ZombieEpidemic
     end
 
     private
-    def render(directory, digits, dpi = 10)
+    def render(directory, digits, dpi = 10, on_console = false)
       map_presenter = MapPresenter.new
       file = File.join(directory, "step_%0#{digits}d.png" % @time)
-      
+
       w = @map.width
       h = @map.height
       map_presenter
         .render(@map)
         .resize(w * dpi, h * dpi)
         .save(file, :fast_rgb)
+
+      if on_console
+        puts `img2txt -H 31 -d none #{file}`
+        sleep 1
+      end
     end
   end
 end
